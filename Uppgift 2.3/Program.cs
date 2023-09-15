d46b6ebcfada032e968f8979ae8238833e1acc80
baronvonpurr <robinhood.svensson@hotmail.com>
2023-09-15 13:20:17 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InlemningsUppgift2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt har du kört i km?");
+            Double Långkört = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många dagar har dy hyrt?");
+            Double Daghyrd = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Du betalar" + (300 + Långkört + Daghyrd * 500) + "kr");
+            Console.ReadKey();
+        }
+    }
+}
